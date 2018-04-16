8cc32b78d3a950f14b7b06f7e1dca98537299e96
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-04-16 18:18:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,20 @@ number *= 10
 console.log(number)
 number /= 5 
 console.log(number)
+
+var number = 10
+function add5() {
+  number +=5
+}
+function divideBy3() {
+  number /=3
+}
+divideby3()
+console.log(number)
+add5()
+console.log(number)
+number = 10
+add5()
+console.log(number)
+divideby3()
+console.log(number)
